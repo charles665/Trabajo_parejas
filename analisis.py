83c5eb7c4e6fcb02e5ba4737c691f9753c2410d7
Hlaxas <miguel.herrera.quintero17@gmail.com>
2025-05-21 15:40:09 -0500

    Registrar y ver proyecto

--- analisis.py
+++ analisis.py
@@ -14,3 +14,19 @@ class Gestor_proyecto:
                 writer = csv.DictWriter(f, fieldnames=self.campos)
                 writer.writeheader()
     
+    def registrar_proyecto(self, nombre, angulo, resultado): #para registrar un proyecto
+        try:
+            longitud = float(longitud)
+        except ValueError:
+            raise ValueError("La longitud debe ser un número.") #para comprobar que la longitud es un numero
+        
+        angulo = random.uniform(30, 60) #angulo aleatorio
+        resultado = math.tan(math.radians(angulo)) * longitud #resultado de la tangente
+        
+        
+    def ver_proyectos(self, numero): #para ver los proyectos
+        df = self.cargar_todos() #para cargar todos los proyectos
+        try:
+            return df.iloc[int(numero)].to_dict() #para ver los proyectos
+        except (ValueError, IndexError):
+            raise ValueError("Número de proyecto no válido.")

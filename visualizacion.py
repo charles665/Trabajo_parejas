f22eb3f8cc79139885df23f4fbfcf24cd49d647f
charles665 <jaguilarestacio@gmail.com>
2025-05-22 00:40:32 -0500

    Termino la parte de elimar y adelanto lo que la edicion de datos

--- visualizacion.py
+++ visualizacion.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import tkinter as tk
@@ -13,55 +14,44 @@ gestor = analisis.Gestor_proyecto()
 
 def mostrar_registrar():
     ventana1 = tk.Toplevel(ventana)
-    ventana1.title('Registro de proyectos')
+    ventana1.title('REGISTRO')
     ventana1.geometry('400x300')
-
     frame_registro = tk.Frame(ventana1, bg="white")
     frame_registro.pack(pady=20)
-
-
     tk.Label(frame_registro, text="Nombre del proyecto:").grid(row=0, column=0, padx=5, pady=5)
     entry_nombre = tk.Entry(frame_registro)
     entry_nombre.grid(row=0, column=1, padx=5, pady=5)
-
-
     tk.Label(frame_registro, text="Longitud:").grid(row=1, column=0, padx=5, pady=5)
     entry_longitud = tk.Entry(frame_registro)
     entry_longitud.grid(row=1, column=1, padx=5, pady=5)
+
     def registrar():
      nombre = entry_nombre.get()
      longitud = entry_longitud.get()
      gestor.registrar_proyecto(nombre, longitud)
      tk.Label(ventana1, text="Proyecto registrado", fg="green").pack()
 
-
     boton_registrar = tk.Button(frame_registro, text="Registrar", command=registrar)
     boton_registrar.grid(row=2, column=1, padx=5, pady=5)
 
 def mostrar_datos():
     ventana2 = tk.Toplevel(ventana)
-    ventana2.title('Resumen de proyectos')
+    ventana2.title('RESUMEN DE TODO')
     ventana2.geometry('600x400')
-
     df = gestor.cargar_todos()
-
     texto = tk.Text(ventana2, wrap='none')
     texto.insert(tk.END, df.to_string(index=False))  
     texto.pack(padx=10, pady=10, fill='both', expand=True)
 
 def mostrar_proyecto():
-
     ventana3 = tk.Toplevel(ventana)
-    ventana3.title("Ver proyecto por número")
+    ventana3.title("VER PROYECTOS")
     ventana3.geometry("400x200")
-
-    tk.Label(ventana3, text="Ingrese el número del proyecto:").pack(pady=10)
+    tk.Label(ventana3, text="ingrese el número del proyecto:").pack(pady=10)
     entry_numero = tk.Entry(ventana3)
     entry_numero.pack(pady=5)
-
     resultado = tk.Label(ventana3, text="")
     resultado.pack(pady=10)
-
     def buscar():
         numero = entry_numero.get()
         try:
@@ -69,16 +59,35 @@ def mostrar_proyecto():
             texto = ""
             for clave, valor in proyecto.items():
                 texto += f"{clave}: {valor}\n"
-            resultado.config(text=texto, fg="black")
+            resultado.config(text=texto)
         except:
             resultado.config(text="Número de proyecto no válido")
 
     tk.Button(ventana3, text="Buscar", command=buscar).pack(pady=5)
 
+def mostrar_eliminar():
+     ventana4 = tk.Toplevel(ventana)
+     ventana4.title("EKIMINAR PROYECTOS")
+     ventana4.geometry("400x200")
+     tk.Label(ventana4, text="Número del proyecto a eliminar:").pack(pady=5)
+     entry_numero = tk.Entry(ventana4)
+     entry_numero.pack(pady=5)
+     resultado = tk.Label(ventana4, text="")
+     resultado.pack(pady=5)
+     def eliminar():
+         numero = entry_numero.get()
+         try:
+             gestor.eliminar_proyecto(numero)
+             resultado.config(text="eliminado")
+         except:
+             resultado.config(text="Ese proyecto no existe")
+     tk.Button(ventana4, text="Eliminar", command=eliminar).pack()
+def mostrar_editar():
+    ventana5 = tk.Toplevel(ventana)
+    ventana5.title("ACTUALIZAR PROYECTOS")
+    ventana5.geometry("400x200")
 
 
-def oe():
-    print('hola')
   
 
 ventana = tk.Tk()#inicio la ventana
@@ -98,9 +107,11 @@ boton2.grid(row=0, column=1, padx=10, pady=10)
 boton3 = tk.Button(frame_botones, text="Mostrar proyecto", font=("Arial", 8), fg="black", bg="grey", width=12, command=mostrar_proyecto)
 boton3.grid(row=0, column=2, padx=10, pady=10)
 
-boton4 = tk.Button(frame_botones, text="Mostrar 4", font=("Arial", 12), fg="black", bg="grey", width=12, command=oe)
+boton4 = tk.Button(frame_botones, text="Mostrar 4", font=("Arial", 12), fg="black", bg="grey", width=12, command=mostrar_eliminar)
 boton4.grid(row=0, column=3, padx=10, pady=10)
 
+boton5 = tk.Button(frame_botones, text="Mostrar 4", font=("Arial", 12), fg="black", bg="grey", width=12, command=mostrar_eliminar)
+boton5.grid(row=0, column=4, padx=10, pady=10)
 
 
 
